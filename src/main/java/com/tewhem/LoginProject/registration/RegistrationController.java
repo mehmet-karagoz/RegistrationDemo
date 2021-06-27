bc3b591ff38db40605461e832fac473c945b4a87
mehmet-karagoz <atleownhem@gmail.com>
2021-06-27 17:01:26 +0300

    created controller

---
+++ src/main/java/com/tewhem/LoginProject/registration/RegistrationController.java
@@ -0,0 +1,13 @@
+package com.tewhem.LoginProject.registration;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+
+}
