fa48b9df82525ddc7a7b1be4dddac3724166dde5
mehmet-karagoz <atleownhem@gmail.com>
2021-06-27 17:01:07 +0300

    filled the method

--- src/main/java/com/tewhem/LoginProject/appuser/AppUserService.java
+++ src/main/java/com/tewhem/LoginProject/appuser/AppUserService.java
@@ -5,14 +5,21 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class AppUserService implements UserDetailsService {
 
+    private final static String USER_NOT_FOUND = "user with email %s not found";
+    private final AppUserRepository appUserRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String arg0)
+    public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        return null;
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format(USER_NOT_FOUND, email)));
     }
 
 }
