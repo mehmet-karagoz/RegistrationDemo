e6a9ac72b00f18b388303bb8866f12b8919d8aeb
mehmet-karagoz <atleownhem@gmail.com>
2021-06-28 13:31:44 +0300

    added some codes inside the method

--- src/main/java/com/tewhem/LoginProject/registration/RegistrationService.java
+++ src/main/java/com/tewhem/LoginProject/registration/RegistrationService.java
@@ -1,12 +1,29 @@
 package com.tewhem.LoginProject.registration;
 
+import com.tewhem.LoginProject.appuser.AppUser;
+import com.tewhem.LoginProject.appuser.AppUserRole;
+import com.tewhem.LoginProject.appuser.AppUserService;
+
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class RegistrationService {
 
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email is not valid.");
+        }
+        return appUserService.signUpUser(new AppUser(request.getFirstName(),
+                request.getLastName(), request.getEmail(),
+                request.getPassword(), AppUserRole.USER));
     }
 
 }
